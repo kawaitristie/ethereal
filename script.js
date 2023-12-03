9405e75af439e157f60f56efbe0a113974d01286
patrickq2h <patrickevoco112@gmail.com>
2023-12-04 02:50:05 +0700

    Timeline dan logo

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function() {
 
     $(window).scroll(function() {
@@ -7,14 +6,4 @@ $(function() {
   
       $('#logoheader').css({'font-size': mass, 'line-height': mass});
     });
-=======
-$(function() {
-
-    $(window).scroll(function() {
-  
-      var mass = Math.max(50, 85-0.1*$(this).scrollTop()) + 'px';
-  
-      $('#logoheader').css({'font-size': mass, 'line-height': mass});
-    });
->>>>>>> d065e843c3ce928a83000d526ef9bf92a523c136
   });
